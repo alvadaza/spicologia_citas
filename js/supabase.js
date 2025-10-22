b6407e544de4e24e8fdd27fc6ba515a3383c337d
github_username <>
2025-10-21 20:56:09 -0500

    vambio de cuenta

--- js/supabase.js
+++ js/supabase.js
@@ -2,8 +2,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // --- CONFIGURA TU SUPABASE ---
-const supabaseUrl = "https://vzxmcaxbqdrxahxfucuj.supabase.co";
+const supabaseUrl = "https://dbhzwjgfknkyiesudnji.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6eG1jYXhicWRyeGFoeGZ1Y3VqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyMzk1NjEsImV4cCI6MjA3MzgxNTU2MX0.iMeANU2EJUb9yZLFCbEYWHFDPzoHsgV59gdJC2O0a2g";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRiaHp3amdma25reWllc3VkbmppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNTIxMjYsImV4cCI6MjA3NjYyODEyNn0.-_SubqCHNLF5QfbqOFAl7jJuemyAOrwLp9WkbbEx5MM";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
